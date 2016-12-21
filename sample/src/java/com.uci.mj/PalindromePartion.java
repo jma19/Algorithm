4cd303e233bf05d211b6eb1d41a7d8eebf6ce4c5
majun <zte_majun@126.com>
2016-12-20 22:25:19 -0800

    add codes

---
+++ sample/src/java/com.uci.mj/PalindromePartion.java
@@ -0,0 +1,7 @@
+package com.uci.mj;
+
+/**
+ * Created by junm5 on 12/18/16.
+ */
+public class PalindromePartion {
+}
---
+++ sample/src/java/com.uci.mj/PlaneManager.java
@@ -0,0 +1,16 @@
+package com.uci.mj;
+
+/**
+ * Created by junm5 on 12/20/16.
+ */
+
+/**
+ * This is for test
+ *
+ * other considerations:
+ * Q1:
+ * Q2
+ */
+public class PlaneManager {
+
+}
